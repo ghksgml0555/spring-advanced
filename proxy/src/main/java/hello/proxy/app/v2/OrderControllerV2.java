f6da921058e6d19cdc07f7a89cb2387a4f4acb99
dsad2221s <ghksgml0555@naver.com>
2023-05-18 16:55:45 +0900

    인터페이스 없는 구체클래스를 스프링빈으로 등록
    인터페이스 없는 구체클래스를 스프링빈으로 등록

---
+++ proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,29 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequestMapping //@Controller를 쓰면 수동등록을 하려고하는데 컴포넌트 스캔대상이 되기때문에 이걸씀
+@ResponseBody
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
